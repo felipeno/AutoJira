0037988064f6f9ec58f55e2d8200ae2d4b225451
Nicole <vieiranicolealice@gmail.com>
2021-03-30 22:52:28 -0300

    Create the configuration layout.
    Need implement calendar button correctly.

--- layouts.py
+++ layouts.py
@@ -12,10 +12,41 @@ def tela_inicial():
     ]
     return sg.Window('Tela inicial', layout=layout, finalize=True)
 
+
+def tela_configuracao():
+    sg.theme('DarkAmber')
+    column_to_be_centred = [
+        [sg.Text('SeniorX')],
+        [sg.Text('Login'), sg.Input()],
+        [sg.Text('Senha'), sg.Input()],
+        [sg.Text('Jira')],
+        [sg.Text('Login'), sg.Input()],
+        [sg.Text('Senha'), sg.Input()],
+        [sg.Checkbox('SALVAR LOGINS')],
+        [
+            sg.Input(key='A', size=(20, 1)),
+            sg.CalendarButton('CONFIGURAÇÃO DE DIAS A SEREM BATIDOS',
+                              close_when_date_chosen=True,
+                              target='A',
+                              location=(0, 0),
+                              no_titlebar=False)
+        ]
+    ]
+
+    layout = [
+        [sg.Button('VOLTAR')],
+        [sg.Column(column_to_be_centred, element_justification='center')]
+    ]
+    return sg.Window('Tela de configuração', layout=layout, finalize=True)
+
+
 # Criar janelas iniciais
 
 
 if __name__ == '__main__':
-    tela_inicial()
+    janela1 = tela_configuracao()
     while True:
-        a,b,c = sg.read_all_windows()
+        evento, valor = janela1.read()
+        print(evento,'******', valor)
+        if evento in (sg.WIN_CLOSED, 'Exit'):
+            break
